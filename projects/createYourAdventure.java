31fb2d71809684c930894d9a2859045a071ed270
Henry Cechini <82913987+Hcech64@users.noreply.github.com>
2022-10-03 13:24:58 -0700

    Finished

--- projects/createYourAdventure.java
+++ projects/createYourAdventure.java
@@ -11,16 +11,16 @@ public class createYourAdventure {
     public static void main (String []args){
         //4 choices per path. must have a while loop to check that the user is inputing correct answers
 
-        System.out.println("You are walking down the street in the big city. You are a little low on money, and your rent was due yesterday. Type \'a\' if you want to accept the consequences of not paying your rent, or type \'b\' if  you want to make a little extra money, or \'c\' if you want to escape to ecuador.");
+        System.out.println("You are walking down the street in the big city. You are a little low on money, and your rent was due yesterday. Type \'1\' if you want to accept the consequences of not paying your rent, or type \'2\' if  you want to make a little extra money, or \'3\' if you want to escape to ecuador.");
         String answer = keyboard.nextLine();
         
-        while(!answer.equals("a") && !answer.equals("b") && !answer.equals("c")){
+        while(!answer.equals("1") && !answer.equals("2") && !answer.equals("3")){
             System.out.println("please enter a vaid answer: ");
             answer = keyboard.nextLine();
         }
 
         //accept consequence
-        if (answer.equals("a")){
+        if (answer.equals("1")){
             System.out.println("when you return home, your land lord is very angry. He tells you to pack your bags, cuase \'YOUR OUTA HERE!\' Type \'a\' to accept the consequences, or type \'b\' if you want to stab your landlord and claim his house as your own");
 
             answer = keyboard.nextLine();
@@ -47,7 +47,6 @@ public class createYourAdventure {
                     else if (answer.equals("b")){
                         System.out.println("You run into the forest and find a nice cave to sleep in. you cubble up against a soft shrub in the cave and fall asleep. In the middle of the night you come to the realization that the shrub was actully a bear when it rips your face off. You die.");
                     }
-
                 }
                 // run from homeless
                 if (answer.equals("b")){
@@ -66,7 +65,6 @@ public class createYourAdventure {
                         
                     }
                 }
-
             }
             //stab landlord
             else if (answer.equals("b")){
@@ -87,9 +85,7 @@ public class createYourAdventure {
                     }
                     if (answer.equals("b")){
                         System.out.println("you decline the offer, but he finds you secret and murders you in your sleep.");
-
                     }
-
                 }
                 //call help
                 else if (answer.equals("b")){
@@ -105,18 +101,10 @@ public class createYourAdventure {
                         System.out.println("you lie to the cops and they find out. You are sent to prison, where you are brutally murdered by a large felon who didnt like the way u ate your broccli");
                     }
                 }
-
-                
-
             }
-           
-
-
-
-
         }
         //need money
-        else if (answer.equals("b")){
+        else if (answer.equals("2")){
             System.out.println("You decide to make a little extra money. You only know of two ways to get money: type \'a\' to choose McDonalds, or type \'b\' to chooseAssasination");
             
             answer = keyboard.nextLine();
@@ -157,96 +145,119 @@ public class createYourAdventure {
             }
             //assasination
             else if (answer.equals("b")){
-                System.out.println("");
+                System.out.println("You get hired as an assasin and are givin your first asignment. You and your parter Tyler are assigned to kill the president of charmin ultrasoft. You feel bad becuase he has done nothing wrong. Type \'a\' to kill him, or type \'b\' to refuse");
                 
                 answer = keyboard.nextLine();
                 answer = checkValid(answer);
 
+                //kill him
                 if (answer.equals("a")){
-                    System.out.println("");
+                    System.out.println("You sneak into his hous and find him asleep in his bead. Now is the moment, type \'a\' to kill him, or type \'b\' to chicken out.");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                            System.out.println("you shoot him in the head, and then are shot from behind as Tyler describes about maximizing profit.");
+                    }
+                    else if (answer.equals("b")){
+                            System.out.println("you chicken out. Tyler finishes the job, then kills you for being \'too weak\'");
+                    }
                 }
+                //refuse
                 else if (answer.equals("b")){
-                    System.out.println("");
+                    System.out.println("You refuse to kill the president and are punished by your assasination group. You have to \'a\' kill his family, or \'b\' get killed");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                        System.out.println("you kill his entire family, but are then murded by the groups becuase you were weak.");
+                    }
+                    else if (answer.equals("b")){
+                         System.out.println("you refuse to kill him and are brutally murded.");
+                    }
                 }
 
             } 
         }
         //ecuador
-        else if (answer.equals("c")){
-            System.out.println("");
+        else if (answer.equals("3")){
+            System.out.println("You buy a ticket to ecuador, but on the way to the airpot a recall in announced for the plane you are about to board. Type \'a\' to board, or \'b\' to wait for another plane.");
             
             answer = keyboard.nextLine();
             answer = checkValid(answer);
 
             if (answer.equals("a")){
-                System.out.println("");
+                System.out.println("you continue on your journey, and the plane takes off. mid flight, the plane starts to go down,the cabin depreasurizes and the masks drop down. Yours does not, but the kid's next to you does. Type \'a\' to try to find another mask, or type \'to take the childs mask.\'");
             
                 answer = keyboard.nextLine();
                 answer = checkValid(answer);
 
                 if (answer.equals("a")){
-                    System.out.println("");
+                    System.out.println("You try to find a mask but to no avail. You know you are going to die. Type \'a\' to jump out the open door, or type \'b\'' to sit down and give up.");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                        System.out.println("you jump out of the plane and the last thing that goes through your mind is that you could have taken that kid's mask.");
+                    }
+                    else if (answer.equals("b")){
+                        System.out.println("you sit down in your chair and slowly fall into a deep sleep. Your body was never found in the crash.");
+                    }
                 }
                 else if (answer.equals("b")){
-                    System.out.println("");
+                    System.out.println("You take the child's mask and he passes out quickly. The plane is still plumiting, whith no chanch of survival. You know you are going to die. Type \'a\'' to jump out the open door, or type \'b\' to sit down and give up.");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                        System.out.println("you jump out the door, and you are super lucky, landing in a cushy tree and only breaking a leg and arm. You think that you can make it, but you are mauled by a hungry bear. You don't last two minutes in its jaws.");
+                    }
+                    else if (answer.equals("b")){
+                        System.out.println("You sit down and think of how side your life is as your vision fades to darkness.");
+                    }
                 }
             }
             else if (answer.equals("b")){
-                System.out.println("");
+                System.out.println("You wait for a new plane to arrive. When it does it is the same kind of plane. Type \'a\' to wait for yet another plane, or type \'b\' to board the plane.");
                 
                 answer = keyboard.nextLine();
                 answer = checkValid(answer);
 
                 if (answer.equals("a")){
-                    System.out.println("");
+                    System.out.println("you wait for a third plane but when it comes, the pilot says it can't take all the passengers that bought a ticket. Type \'a\' to get a refund and give up on Ecuador, or type \'b\' to let someone else take the bump.");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                        System.out.println("You give up on going to ecuador. You are sad and hopeless and not paying attention to the road as you drive home. Your car runs out of gas as rolled to a stop on some traintracks. You didnt see the train.");
+                    }
+                    else if (answer.equals("b")){
+                        System.out.println("You let someone else take the bump, but as you the plane takes off, it hits a bird in one of the jets, sending it into a tailspin. there were no survivors.");
+                    }
                 }
                 else if (answer.equals("b")){
-                    System.out.println("");
+                    System.out.println("you board the plane, and can immediatly tell that it was a bad discision. The plane ride is super rought and you get sick to your stomach. Type \'a\' to go to the bathroom, or type \'b\' to stay seated.");
                 
                     answer = keyboard.nextLine();
                     answer = checkValid(answer);
 
-                    if (answer.equals("a")){}
-                    else if (answer.equals("b")){}
+                    if (answer.equals("a")){
+                        System.out.println(" you go the the bathroom, but the cabin looses preasure and you pass out. Your body was never found in the crash.");
+                    }
+                    else if (answer.equals("b")){
+                        System.out.println("you stay seated and the plane ride smooths out. Just before you land, one of the passengers pulls our a gun and shooths the pilots through the cabin door. He then jumps out the door with a parashoot. No one survived the crash.");
+                    }
                 }
 
             } 
         }
 
-
         }
-        
-    }
 
     public static String checkValid(String ans) {
         
@@ -257,3 +268,5 @@ public class createYourAdventure {
         return ans;
     }
 }
+
+
