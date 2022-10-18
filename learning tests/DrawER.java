cede42486711b8e8c1f222afed785febb74264c9
Henry Cechini <82913987+Hcech64@users.noreply.github.com>
2022-10-18 13:05:31 -0700

    Create DrawER.java

---
+++ learning tests/DrawER.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+
+public class DrawER extends JComponent{
+    public static void main(String[] args){
+
+       // Scanner keyboard = new Scanner(System.in);
+
+       JFrame frame = new JFrame();
+       //Drawing shapes = new Drawing();
+
+       frame.setSize(200,200);
+       frame.setTitle("Piktur");
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+       frame.setVisible(true);
+
+    
+    }    
+}
