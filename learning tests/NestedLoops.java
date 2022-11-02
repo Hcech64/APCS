3ce274b2d35d8f32840b7724ef566e6b3f1a89e5
Henry Cechini <82913987+Hcech64@users.noreply.github.com>
2022-11-02 09:56:22 -0700

    Create NestedLoops.java

---
+++ learning tests/NestedLoops.java
@@ -0,0 +1,15 @@
+
+public class NestedLoops {
+    
+public static void main (String [] args){
+
+    int x = 0;
+    int y = 0;
+
+    for(y=0; y < 5; y++){
+        for(x=0; x<=y; x++);
+        System.out.print("*");
+        }
+         System.out.println("*");
+    }
+}
