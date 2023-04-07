0f01e01f40c06b466cb5a059bbc17c7906e61f60
Hcech64 <Hcechini@lshigh.org>
2023-04-07 02:47:01 -0700

    Update MonteCarlo.java

--- learning tests/review_programs/MonteCarlo.java
+++ learning tests/review_programs/MonteCarlo.java
@@ -15,26 +15,27 @@ public class MonteCarlo {
         System.out.println("how many random coordinates do you want?");
         int coords = k.nextInt();
 
-        int [][] coordinates = new int[coords][2]; 
+        double [][] coordinates = new double[coords][2]; 
 
         for(int i = 0; i < coords; i++){
-            coordinates[i][0] = (int)(Math.random()*side);
-            coordinates[i][1] = (int)(Math.random()*side);
+            coordinates[i][0] = (Math.random()*side);
+            coordinates[i][1] = (Math.random()*side);
         }
 
         for(int i = 0; i < coords; i++){
-            System.out.println("Corrrdinate " + (i+1) + " is (" + coordinates[i][0] +", " +  coordinates[i][1] + ")");
+           //System.out.println("Corrrdinate " + (i+1) + " is (" + coordinates[i][0] +", " +  coordinates[i][1] + ")");
         }
         
         int count = 0;
         for(int i =0; i < coordinates.length-1; i++){
             if(Math.sqrt(coordinates[i][0]*coordinates[i][0] + coordinates[i][1]*coordinates[i][1]) > side/2){
-                System.out.println("Coordinate " + (i+1) + " is not in the circle");
+                //System.out.println("Coordinate " + (i+1) + " is not in the circle");
                 count++;
             }
         }
 
-        System.out.println("the ratio of coordinate that are inside the circle and outside is" + (coords-count) + ":" + (count));
+        System.out.println("the ratio of coordinate that are inside the circle and outside is " + (coords-count) + ":" + (count));
+        System.out.println(1 - (coords-count)/(double)count);
 
         k.close();
 
